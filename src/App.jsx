9356329b17bc0dd722701aa306047fe6b3072fc5
DEVANSH ARYAN <1ds22cb022@dsce.edu.in>
2025-02-12 08:47:13 +0530

    custom hook generated

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+   
+  
+
+  return (
+    <div>
+      <Counter />
+      <Counter />
+    </div>
+  )
+}
+
+
+function useCounter(){
+  const [count, setCount] = useState(0);
+    
+  function increaseCount(){
+    setCount(count + 1)
+  }
+  return {
+    count : count,
+    increaseCount : increaseCount
+  }
+}
+
+function Counter(){
+  const {count , increaseCount} = useCounter()
+   return <div>
+    <button onClick={increaseCount}>Increase {count}</button>
+   </div>
+}
+
+export default App
